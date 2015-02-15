9df362cddd6ac0b2fc13df03a1cbf1083c6b69df
Gruntin <domsanteramo@gmail.com>
2015-02-14 20:43:02 -0500

    Revert "Implemented A3G Spectator Script"
    This reverts commit ec0a9f1413b3747543fb21892573c3397360bc44.

--- Ph3.VR/a3g-spectatorcam/CfgFunctions.hpp
+++
@@ -1,9 +0,0 @@
-class A3GSC {
-	class Functions {
-		file = "a3g-spectatorcam";
-		class handleVisionSwitch {};
-		class handleCameraSwitch {};
-		class calcVecDir {};
-		class serverInfo {postInit = 1};
-	};
-};
--- Ph3.VR/a3g-spectatorcam/fn_handleVisionSwitch.sqf
+++
@@ -1,5 +0,0 @@
-switch (A3G_SpectatorCamVisionMode) do {
-	case 0: {false setCamUseTi 0};
-	case 1: {camUseNVG true};
-	case 2: {camUseNVG false; true setCamUseTi 0};
-};
